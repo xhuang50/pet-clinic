0668c8c5ae92fc409aafb2cf0f4b3609552aa50e
Xin Huang <xhuang0314@gmail.com>
2021-03-10 19:37:07 -0600

    Add Base Entity

--- pet-clinic-data/src/main/java/indoor/incubator/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/indoor/incubator/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package indoor.incubator.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/indoor/incubator/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/indoor/incubator/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package indoor.incubator.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
