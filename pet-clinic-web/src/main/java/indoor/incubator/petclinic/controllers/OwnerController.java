3ee85b24a3408bdc056e442d04fe98eeb831ddb1
Xin Huang <xhuang0314@gmail.com>
2021-04-02 16:50:40 -0500

    Add Owner List

--- pet-clinic-web/src/main/java/indoor/incubator/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/indoor/incubator/petclinic/controllers/OwnerController.java
@@ -1,14 +1,22 @@
 package indoor.incubator.petclinic.controllers;
 
+import indoor.incubator.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
