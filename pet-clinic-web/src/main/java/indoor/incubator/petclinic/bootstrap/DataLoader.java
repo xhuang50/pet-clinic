7b8a157477def92f6bb071c717f1ff6d8ef6afe4
Xin Huang <xhuang0314@gmail.com>
2021-04-02 17:47:15 -0500

    Add auto index generation

--- pet-clinic-web/src/main/java/indoor/incubator/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/indoor/incubator/petclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Alica");
         owner1.setLastName("Apex");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Bela");
         owner2.setLastName("Benjamin");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Alex");
         vet1.setLastName("Alivet");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Beta");
         vet2.setLastName("Betavet");
 
